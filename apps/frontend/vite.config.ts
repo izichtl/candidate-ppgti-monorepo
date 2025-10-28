455a6b20b972537b123cc95544e4670d0fe2a654
Ivan Zichtl <izichtl@gmail.com>
2025-10-28 16:14:25 -0300

    main(main): updated

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,13 +1,27 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',
+//     port: 5173,
+//   },
+//   build: {
+//     outDir: 'dist',
+//   },
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/inscricoesppgti/',
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: true,
     port: 5173,
   },
-  build: {
-    outDir: 'dist',
+  preview: {
+    port: 80,
   },
 });
